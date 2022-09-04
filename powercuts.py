efb8eb6c3ed49d04de240f51bc84d33dd0393d94
markgolubev <golubev@ucheba.ru>
2022-09-04 23:35:11 +0400

    test output

--- powercuts.py
+++ powercuts.py
@@ -2,13 +2,17 @@ import json
 
 
 def search_by_postcode(postcode):
-    serchedDict = dict()
+    serchedList = []
+    uniqueSet = set()
     with open('test_jsons/lists.json') as f:  # Использовать джейсон из редиса, который обновляется по апишке раз в 10 минут
         templates = json.load(f)
         for incident in templates['incidents']:
             if incident['ukpnIncident'] is not None:
                 for postCodeAffected in incident['ukpnIncident']['postCodesAffected'] + incident['ukpnIncident']['postCodesPlanned']:
-                    if postcode.replace(" ", "") in postCodeAffected.replace(" ", ""):
-                        serchedDict[incident['incidentReference']] = incident
+                    if postcode.replace(" ", "").lower() in postCodeAffected.replace(" ", "").lower():
+                        # uniqueSet.add(incident['incidentReference'])
+                        # if incident['incidentReference'] not in uniqueSet:
+                        serchedList.append(incident)
+                        break
 
-    return serchedDict
+    return serchedList
