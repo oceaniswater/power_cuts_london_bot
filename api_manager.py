6a434c8382f4164b9fc408a1cf21c5ccc4dd1226
markgolubev <golubev@ucheba.ru>
2022-09-07 21:37:24 +0400

    test bold

--- api_manager.py
+++ api_manager.py
@@ -19,11 +19,11 @@ class ApiPowerCuts:
         return result
 
     @staticmethod
-    async def get_incident_by_id(incident_id):
+    def get_incident_by_id(incident_id):
         url = f'https://www.ukpowernetworks.co.uk/api/power-cut/incident-by-id?id={incident_id}'
         result = requests.request("GET", url)
         print(result.status_code)
-        await result
+        return result
 
     @staticmethod
     def get_fetch_list():
